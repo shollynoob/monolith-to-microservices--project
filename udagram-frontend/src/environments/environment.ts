dda776bf6dfead31b11f96da19d086cfdd766300
Olusola <doolatunde@gmail.com>
2022-09-25 22:57:45 +0000

    Refac and New AWS Enviroment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a1c27c70b7b97476e93c75e0d1e99d77-447381598.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
